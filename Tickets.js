3a075313cf6ca54b8ad7b88a0cd98caaa17c346d
Elizabeth Adeotun Adegbaju <adeotunadegbaju@gmail.com>
2022-01-22 11:18:09 +0100

    feat: add navigation

---
+++ Tickets.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
+import globoTickets from "./TicketsDB";
+
+const Tickets = () => {
+    const ticketItem = ({ item }) => {
+        return (
+            <View style={styles.tickets}>
+                <View>
+                    <Image
+                        style={styles.img}
+                        source={item.image}
+                    />
+                </View>
+                <View>
+                    <Text
+                        style={styles.ticketTitle}
+                    >
+                        {item.event}
+                    </Text>
+                    <Text
+                        style={styles.ticketShortDescription}
+                    >
+                        {item.shortDescription}
+                    </Text>
+                    <Text
+                        style={styles.ticketDescription}
+                        numberOfLines={2}
+                        ellipsizeMode="tail"
+                    >
+                        {item.description}
+                    </Text>
+                    <Text
+                        style={styles.ticketShortDescription}
+                    >
+                        Prices: ${item.price}
+                    </Text>
+                    <Text
+                        style={styles.ticketButton}
+                    >
+                        GET TICKETS
+                    </Text>
+                </View>
+            </View>
+        );
+    };
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={globoTickets}
+                renderItem={ticketItem}
+                keyExtractor={(item) => item.eventId}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 15,
+        paddingBottom: 15
+    },
+    tickets: {
+        flexDirection: "column"
+    },
+    img: {
+        height: 180,
+        width: "100%"
+    },
+    ticketTitle: {
+        fontFamily: "Ubuntu-Regular",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    ticketShortDescription: {
+        fontFamily: "Ubuntu-Light",
+        fontWeight: "600",
+        textAlign: "center",
+        paddingTop: 5
+    },
+    ticketDescription: {
+        fontFamily: "Ubuntu-Light",
+        fontWeight: "600",
+        padding: 15
+    },
+    ticketButton: {
+        fontFamily: "Ubuntu-Regular",
+        fontWeight: "bold",
+        textAlign: "center",
+        paddingTop: 5,
+        paddingBottom: 15
+    }
+});
+
+export default Tickets;
+
---
+++ TicketsDB.js
@@ -0,0 +1,46 @@
+const globoTickets = [
+    {
+        event: 'DJ Conference',
+        image: require('./images/logo_large.png'),
+        eventId: 'DJ-1',
+        description: `DJ conference for those looking to get in the industry.
+        Virtual due to the global situation. Sign up to be a part of all the excitement as vendors will be on hand to demo the latest gear`,
+        shortDescription: 'A virtual DJ conference like no other',
+        price: 20.00
+    },
+    {
+        event: 'Smart Guy',
+        image: require('./images/logo_large.png'),
+        eventId: 'SG-1',
+        description: `Smart Guy has a talk where he explains how he made his fortune. He will discuss the methods he used on his rise to greatness. He still also explains how to set up your business the right way.`,
+        shortDescription: 'Smart Guy tells you how it is',
+        price: 100.00
+    },
+    {
+        event: 'Guitar Gary meet and greet',
+        image: require('./images/logo_large.png'),
+        eventId: 'GT-1',
+        description: `Meet and interact with the award winning musician Guitar Gary.
+        Ask the questions you always wanted to know. How did he write his biggest hits. how did he get started. Get your tickets`,
+        shortDescription: 'Meet the one and only Guitar Gary',
+        price: 45.00
+    },
+    {
+        event: 'Big Tech Summit',
+        image: require('./images/logo_large.png'),
+        eventId: 'BT-1',
+        description: `Join the leaders of Big TEch as they explain their future plans for world domination. Well maybe not domination but they will talk about their new products and apps of the future`,
+        shortDescription: 'Big Tech can only get bigger',
+        price: 0.00
+    },
+    {
+        event: 'Acoustic Kittens Live',
+        image: require('./images/logo_large.png'),
+        eventId: 'AK-1',
+        description: `A virtual concert for the Acoustic Kittens. Fresh off the release of their new album they will perform all their hits, old and new. This is sure to be a great time for all.`,
+        shortDescription: 'Acoustic Kittens in concert',
+        price: 60.00
+    }
+]
+
+export default globoTickets;
